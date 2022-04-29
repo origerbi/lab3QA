d89d96cd5397ab5dbe5727bc6786818372388893
ShiningBlack <amitzxzx@gmail.com>
2022-04-29 13:28:21 +0300

    Update MyReviewsPage.java

--- src/test/java/pages/MyReviewsPage.java
+++ src/test/java/pages/MyReviewsPage.java
@@ -2,6 +2,8 @@ package pages;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MyReviewsPage {
 	private WebDriver driver;
@@ -9,6 +11,9 @@ public class MyReviewsPage {
 	public MyReviewsPage(WebDriver driver) {
 		this.driver = driver;
 	}
+
+	@FindBy(xpath = "/html/body/div[1]/div/form/div[3]/div/div[2]/div[1]/div/div/div[3]/textarea") WebElement reviewText;
+	@FindBy(xpath = "/html/body/div[1]/div/form/div[3]/div/div[1]/div") WebElement saveReview;
 	
     public void writeReview(int star, String text, Logger logger) throws InterruptedException {
         driver.get("https://www.ticketor.com/demo/myreviews");
@@ -17,8 +22,8 @@ public class MyReviewsPage {
 			logger.warn("star out of bounds");
 		star = 6-star;
         driver.findElement(By.xpath("/html/body/div[1]/div/form/div[3]/div/div[2]/div[1]/div/div/div[1]/label[" + star + "]")).click();
-        driver.findElement(By.xpath("/html/body/div[1]/div/form/div[3]/div/div[2]/div[1]/div/div/div[3]/textarea")).sendKeys(text);
-        driver.findElement(By.xpath("/html/body/div[1]/div/form/div[3]/div/div[1]/div")).click(); // click to save review
+        reviewText.sendKeys(text);
+        saveReview.click(); // click to save review
 		Thread.sleep(1500);
     }
 
