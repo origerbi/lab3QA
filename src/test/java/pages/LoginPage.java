b289eeae3cfadb6db8cfe7c8d3a71e003a0a7c0b
origerbi <origerbi19@gmail.com>
2022-04-29 14:58:08 +0300

    Login add to By

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LoginPage {
 	private WebDriver driver;
@@ -10,17 +12,22 @@ public class LoginPage {
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 	}
+	
+	@FindBy(xpath = "/html/body/div[1]/div/header/nav/ul/li[3]/a") WebElement loginButton;
+	@FindBy(xpath = "/html/body/div[7]/div[2]/form/div[4]/input") WebElement emailField;
+	@FindBy(xpath = "/html/body/div[7]/div[2]/form/div[16]/div[1]/input") WebElement passwordField;
+	@FindBy(xpath = "/html/body/div[7]/div[2]/form/div[18]/button") WebElement signInLogin;
+	@FindBy(xpath = "/html/body/div[1]/div/header/nav/ul/li[3]/a") WebElement signInConfirmation;
 
     public String login(String email, String password, Logger logger) throws InterruptedException {
         driver.get("https://www.ticketor.com/demo/default");
         logger.info("Navigated to Ticketor main page");
-    	driver.findElement(By.xpath("/html/body/div[1]/div/header/nav/ul/li[3]/a")).click();
-        driver.findElement(By.xpath("/html/body/div[7]/div[2]/form/div[4]/input")).sendKeys(email);
-        driver.findElement(By.xpath("/html/body/div[7]/div[2]/form/div[16]/div[1]/input")).sendKeys(password);
-        driver.findElement(By.xpath("/html/body/div[7]/div[2]/form/div[18]/button")).click();
+        loginButton.click();
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+        signInLogin.click();
         logger.info("login preformed");
         Thread.sleep(500);
-        String loginElement = driver.findElement(By.xpath("/html/body/div[1]/div/header/nav/ul/li[3]/a")).getText().trim();
-        return loginElement;
+        return signInConfirmation.getText().trim();
     }
 }
