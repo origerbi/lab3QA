e687c3ba89ee6cb1e7847066e6179b990cd85bdf
origerbi <origerbi19@gmail.com>
2022-05-01 16:52:47 +0300

    Remove uncessry imports

--- src/test/java/testCases/AddItemToCart_TC.java
+++ src/test/java/testCases/AddItemToCart_TC.java
@@ -1,9 +1,6 @@
 package testCases;
 
 import java.io.IOException;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Reporter;
